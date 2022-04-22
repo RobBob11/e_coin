9a78aa87d90e42069b9960b99382ffb50c2af02e
EdwinReyes009 <reyesmorales56@gmail.com>
2022-04-22 11:53:11 -0500

    news_done

--- lib/pages/news.dart
+++ lib/pages/news.dart
@@ -1,69 +1,73 @@
+import 'package:e_coin/models/noticias.dart';
+import 'package:e_coin/services/news_data.dart';
+import 'package:e_coin/widgets/customAppbar.dart';
+import 'package:e_coin/widgets/lista_news.dart';
+import 'package:e_coin/widgets/no_connection.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-import '../models/noticias.dart';
-import '../widgets/customAppbar.dart';
-import '../widgets/lista_news.dart';
-
-final _URL =
-    'https://newsapi.org/v2/everything?q=cripto+bitcoin+criptomonedas&language=es&sortBy=publishedAt';
-final _APIKEY = '407215122500484e9fdd8792c9cc1b67';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class News extends StatefulWidget {
+  const News({Key? key}) : super(key: key);
+
   @override
   State<News> createState() => _NewsState();
 }
 
 class _NewsState extends State<News> with AutomaticKeepAliveClientMixin {
+  // intancia de la clase para el servicio
+  NewsData newsData = NewsData();
   // lista noticias
-  List<Article> news = [];
-  // fechas para las noticias
-  // fecha actual completa
-  var _currentDate;
-  // para armar
-  var _year;
-  var _month;
-  var _day;
+  late Future<List<Article>?> data;
+  // internet checker
+  bool internetState = true;
 
   @override
   void initState() {
-    _currentDate = DateTime.now().toIso8601String();
-    _year = DateTime.now().year;
-    _month = DateTime.now().month;
-    _day = DateTime.now().day;
-    reloadNews();
     super.initState();
+    // call internet connection checker method
+    InternetConnectionChecker().onStatusChange.listen((status) {
+      final hasInternet = (status == InternetConnectionStatus.connected);
+      setState(() {
+        internetState = hasInternet;
+      });
+    });
+    data = newsData.getNews();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarspl("Noticias"),
-      body: RefreshIndicator(
-        onRefresh: reloadNews,
-        child:
-            Container(margin: EdgeInsets.all(10.0), child: ListaNoticias(news)),
-      ),
-    );
-  }
-
-  // traer las noticias
-  Future reloadNews() async {
-    try {
-      final url = Uri.parse(
-          '$_URL&from=$_year-$_month-00&to=$_currentDate&apiKey=$_APIKEY');
-      final response = await http.get(url);
-      final data = modeloNoticiaFromJson(response.body);
-      news = [];
-      if (this.mounted) {
-        setState(() {
-          news.addAll(data.articles);
-        });
-      }
-      print(news.length);
-    } catch (e) {
-      print(e);
-    }
+        appBar: AppBarspl("Noticias", context),
+        // check for internet connection
+        body: (internetState == true)
+            ? FutureBuilder<List<Article>?>(
+                future: data,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    final error = snapshot.error;
+                    return Text('bad');
+                  } else if (snapshot.hasData) {
+                    List<Article>? news = snapshot.data;
+                    return RefreshIndicator(
+                      onRefresh: () async {
+                        setState(() {
+                          data = newsData.getNews();
+                        });
+                      },
+                      child: Container(
+                          margin: const EdgeInsets.all(10.0),
+                          child: ListaNoticias(news!)),
+                    );
+                  } else {
+                    return const Center(
+                        child: Image(
+                      image: AssetImage('assets/img/loadingDataTr.gif'),
+                      width: 80,
+                    ));
+                  }
+                },
+              )
+            : const NoConnection());
   }
 
   @override
