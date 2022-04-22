dfdb520f12468af80d67b2a7152fad9ae7e42373
RobBob11 <robbobnavarro11@gmail.com>
2022-04-22 11:56:02 -0500

    Main al 100

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
 import 'package:e_coin/pages/navigation.dart';
-import 'package:e_coin/services/coins.dart';
+import 'package:e_coin/services/chart_data.dart';
+import 'package:e_coin/theme/theme_provider.dart';
+import 'package:e_coin/widgets/pre_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ServiceHome())],
-      child: MaterialApp(
-        title: 'E-coin',
-        debugShowCheckedModeBanner: false,
-        home: Navigation(),
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeNotifier()),
+          ChangeNotifierProvider(create: (_) => ChartService()),
+        ],
+        child: Consumer<ThemeNotifier>(
+          builder: (context, ThemeNotifier themeNotifier, child) {
+            return (themeNotifier.isLoading!)
+                ? const LoadingTheme()
+                : MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: "E-coin",
+                    theme: themeNotifier.darkTheme! ? dark : light,
+                    home: Navigation(),
+                  );
+          },
+        ));
   }
 }
